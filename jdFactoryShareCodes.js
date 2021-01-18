36d42ec75ba9236074106bbeb970a7ed19a2e69e
朱利云 <fd@qq.com>
2021-01-18 10:54:00 +0800

    update

--- jdFactoryShareCodes.js
+++ jdFactoryShareCodes.js
@@ -7,24 +7,29 @@
 // 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
 let shareCodes = [
-  '',//账号一的好友shareCode,不同好友中间用@符号隔开
-  '',//账号二的好友shareCode，不同好友中间用@符号隔开
-]
+  "T0225KkcRh0b8FfVIxvzlvAOcgCjVWnYaS5kRrbA@T0225KkcRx1I8wfTI0z0nf8CcACjVWnYaS5kRrbA@T0205KkcAU9GqgC9YF-97J5CCjVWnYaS5kRrbA@T0225KkcRxocoVTQJRull6VedQCjVWnYaS5kRrbA@T016Z3bbmIGAIONr9JNPCjVWnYaS5kRrbA@T0205KkcHUpRrgGWc3yC14JSCjVWnYaS5kRrbA@T0205KkcHntDvzOmXkWd7q9uCjVWnYaS5kRrbA", //账号一的好友shareCode,不同好友中间用@符号隔开
+  "T0225KkcRh0b8FfVIxvzlvAOcgCjVWnYaS5kRrbA@T0225KkcRx1I8wfTI0z0nf8CcACjVWnYaS5kRrbA@T0205KkcAU9GqgC9YF-97J5CCjVWnYaS5kRrbA@T0225KkcRxocoVTQJRull6VedQCjVWnYaS5kRrbA@T016Z3bbmIGAIONr9JNPCjVWnYaS5kRrbA@T0205KkcHUpRrgGWc3yC14JSCjVWnYaS5kRrbA@T0205KkcHntDvzOmXkWd7q9uCjVWnYaS5kRrbA", //账号一的好友shareCode,不同好友中间用@符号隔开
+  "T0225KkcRh0b8FfVIxvzlvAOcgCjVWnYaS5kRrbA@T0225KkcRx1I8wfTI0z0nf8CcACjVWnYaS5kRrbA@T0205KkcAU9GqgC9YF-97J5CCjVWnYaS5kRrbA@T0225KkcRxocoVTQJRull6VedQCjVWnYaS5kRrbA@T016Z3bbmIGAIONr9JNPCjVWnYaS5kRrbA@T0205KkcHUpRrgGWc3yC14JSCjVWnYaS5kRrbA@T0205KkcHntDvzOmXkWd7q9uCjVWnYaS5kRrbA", //账号一的好友shareCode,不同好友中间用@符号隔开
+  "T0225KkcRh0b8FfVIxvzlvAOcgCjVWnYaS5kRrbA@T0225KkcRx1I8wfTI0z0nf8CcACjVWnYaS5kRrbA@T0205KkcAU9GqgC9YF-97J5CCjVWnYaS5kRrbA@T0225KkcRxocoVTQJRull6VedQCjVWnYaS5kRrbA@T016Z3bbmIGAIONr9JNPCjVWnYaS5kRrbA@T0205KkcHUpRrgGWc3yC14JSCjVWnYaS5kRrbA@T0205KkcHntDvzOmXkWd7q9uCjVWnYaS5kRrbA", //账号一的好友shareCode,不同好友中间用@符号隔开
+  "T0225KkcRh0b8FfVIxvzlvAOcgCjVWnYaS5kRrbA@T0225KkcRx1I8wfTI0z0nf8CcACjVWnYaS5kRrbA@T0205KkcAU9GqgC9YF-97J5CCjVWnYaS5kRrbA@T0225KkcRxocoVTQJRull6VedQCjVWnYaS5kRrbA@T016Z3bbmIGAIONr9JNPCjVWnYaS5kRrbA@T0205KkcHUpRrgGWc3yC14JSCjVWnYaS5kRrbA@T0205KkcHntDvzOmXkWd7q9uCjVWnYaS5kRrbA", //账号一的好友shareCode,不同好友中间用@符号隔开
+];
 // 判断github action里面是否有东东工厂互助码
 if (process.env.DDFACTORY_SHARECODES) {
-  if (process.env.DDFACTORY_SHARECODES.indexOf('&') > -1) {
-    console.log(`您的互助码选择的是用&隔开\n`)
-    shareCodes = process.env.DDFACTORY_SHARECODES.split('&');
-  } else if (process.env.DDFACTORY_SHARECODES.indexOf('\n') > -1) {
-    console.log(`您的互助码选择的是用换行隔开\n`)
-    shareCodes = process.env.DDFACTORY_SHARECODES.split('\n');
+  if (process.env.DDFACTORY_SHARECODES.indexOf("&") > -1) {
+    console.log(`您的互助码选择的是用&隔开\n`);
+    shareCodes = process.env.DDFACTORY_SHARECODES.split("&");
+  } else if (process.env.DDFACTORY_SHARECODES.indexOf("\n") > -1) {
+    console.log(`您的互助码选择的是用换行隔开\n`);
+    shareCodes = process.env.DDFACTORY_SHARECODES.split("\n");
   } else {
     shareCodes = process.env.DDFACTORY_SHARECODES.split();
   }
 } else if (process.env.DDFACTORY_SHARECODES) {
-  console.log(`由于您secret里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
+  console.log(
+    `由于您secret里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`
+  );
 }
 for (let i = 0; i < shareCodes.length; i++) {
-  const index = (i + 1 === 1) ? '' : (i + 1);
-  exports['shareCodes' + index] = shareCodes[i];
+  const index = i + 1 === 1 ? "" : i + 1;
+  exports["shareCodes" + index] = shareCodes[i];
 }
